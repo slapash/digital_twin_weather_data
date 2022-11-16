77afbe9fff69703e04ce86fa8d58f49469bb6327
Omar Benaidy <benaidy.omar@gmail.Com>
2022-11-16 22:55:09 +0100

    Add files via upload

---
+++ doc(java)/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"com.example","l":"Communication"},{"p":"com.example","l":"Insertion"},{"p":"com.example","l":"Main"}];updateSearchResults();
